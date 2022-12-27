2932035d6a4990210624cccd8e801d45f8fc183c
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-12-27 22:27:51 +0530

    feat: added rest of the tests for both asymmetric and symmetric encryption.

---
+++ README.md
@@ -0,0 +1,13 @@
+### Cryption
+
+**"Cryption"** is a higher level wrapper around the `crypto.subtle` API. It provides some convenience methods for encrypting and decrypting data for your regular developer to understand in his daily life. It just works. : )
+
+### Development
+
+- `yarn install` to install all dependencies.
+- `yarn test` to run the tests.
+
+### Sources
+If you want to know more about the `crypto.subtle` API, please check the
+- [MDN](https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto) documentation.
+- [W3C](https://www.w3.org/TR/WebCryptoAPI/) documentation.
--- tests/symmetric.test.ts
+++ tests/symmetric.test.ts
@@ -1,5 +1,6 @@
 import { generateAESKey, encryptAES, decryptAES } from "../core/symmetric";
 import { export_symmetric_key, fromBufferTo } from "../helpers/utils";
+import * as crypto from "crypto";
 
 describe("Symmetric Algorithms Test", () => {
 
@@ -12,7 +13,7 @@ describe("Symmetric Algorithms Test", () => {
         });
 
         test("AES-GCM Encryption and Decryption with Text", async () => {
-            const iv = new Uint8Array(96);
+            const iv = crypto.getRandomValues(new Uint8Array(96));
             const key = await generateAESKey(256, true, 'AES-GCM');
             const text = "Hello World";
 
@@ -27,6 +28,56 @@ describe("Symmetric Algorithms Test", () => {
             expect(decryptedText).toBeDefined();
             expect(decryptedText).toBe(text);
         });
+    });
+    describe("AES-CBC", () => {
 
+        test("AES-CBC Key Generation and Export To Base64", async () => {
+            const key = await generateAESKey(256, true, 'AES-CBC');
+            const exportedKey = await export_symmetric_key(key);
+            expect(exportedKey).toBeDefined();
+
+        });
+
+        test("AES-CBC Encryption and Decryption with Text", async () => {
+            const iv = crypto.getRandomValues(new Uint8Array(16));
+            const key = await generateAESKey(256, true, 'AES-CBC');
+            const text = "Hello World";
+
+            const encrypted = await encryptAES(key, text, "AES-CBC", iv);
+            const decrypted = await decryptAES(key, encrypted, "AES-CBC", iv);
+
+            const encryptedText = await fromBufferTo(encrypted, "base64");
+            const decryptedText = await fromBufferTo(decrypted, "utf8");
+
+            expect(encryptedText).toBeDefined();
+            expect(decryptedText).toBeDefined();
+            expect(decryptedText).toBe(text);
+
+        });
+    });
+    describe("AES-CTR", () => {
+
+        test("AES-CTR Key Generation and Export To Base64", async () => {
+            const key = await generateAESKey(256, true, 'AES-CTR');
+            const exportedKey = await export_symmetric_key(key);
+            expect(exportedKey).toBeDefined();
+        });
+
+        test("AES-CTR Encryption and Decryption with Text", async () => {
+            const counter = crypto.getRandomValues(new Uint8Array(16));
+            const key = await generateAESKey(256, true, 'AES-CTR');
+            const text = "Hello World";
+
+            const encrypted = await encryptAES(key, text, "AES-CTR", undefined, counter);
+            const decrypted = await decryptAES(key, encrypted, "AES-CTR", undefined, counter);
+
+            const encryptedText = await fromBufferTo(encrypted, "base64");
+            const decryptedText = await fromBufferTo(decrypted, "utf8");
+
+            expect(encryptedText).toBeDefined();
+            expect(decryptedText).toBeDefined();
+            expect(decryptedText).toBe(text);
+
+        });
     });
 });
